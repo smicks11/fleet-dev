ad413df13f7a91be70eac5214fbce78ef1f84b45
Smicks <great@greats-MacBook-Pro.local>
2025-02-27 15:46:54 +0100

    deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2574264948"
+    serviceWorkerVersion: "2149446915"
   }
 });
